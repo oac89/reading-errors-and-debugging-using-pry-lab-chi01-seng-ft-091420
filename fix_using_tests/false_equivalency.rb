d7b64ab6e9fb2e7a1e8a2b1b77da3993cb65e7ee
Oscar A Castro <oacastro2@gmail.com>
2020-09-11 23:27:16 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,24 +1,8 @@
-# don't forget to add: require 'pry'
-
-def get_user_input
-  gets.chomp
-end
-
-def prompt_user
-  puts "What would you like to do?"
-  puts "1.) Eat a hamburger."
-  puts "2.) Eat a ham."
-end
-
 def selection(num)
-  if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
-  end
-end
+  if num == 1
+     p "YUM YUM MUNCH MUNCH MUNCH"
+  elsif num == 2
+     p "HAM HAM HAM IN MY TUMMY"
 
-def runner
-  prompt_user
-  selection(get_user_input)
+  end
 end
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,12 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
+  #binding.pry
   if string[0] == "s"
-    10 * "s" + string
+    10.times "s"
+    
   else
-    string
+    puts string
   end
+  string 
 end
